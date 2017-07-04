aa098a848e0f2eec342715514d47b152f63abbe7
albertocuberes <albertocubkolau@gmail.com>
2017-07-04 22:48:16 +0200

    fixed comentarios

---
+++ db/migrate/20170704203130_add_ponercolumnas_to_comentarios.rb
@@ -0,0 +1,6 @@
+class AddPonercolumnasToComentarios < ActiveRecord::Migration[5.0]
+  def change
+	add_column :comentarios, :name, :string
+  	add_column :comentarios, :pelicula_id, :integer
+  end
+end
