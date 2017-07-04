c34f8c3278f1055e95d8b2068c92915dce945ed6
albertocuberes <albertocubkolau@gmail.com>
2017-07-04 16:57:29 +0200

    añadido fixed 8

--- db/migrate/20170529083245_create_peliculas.rb
+++ db/migrate/20170529083245_create_peliculas.rb
@@ -3,9 +3,17 @@ class CreatePeliculas < ActiveRecord::Migration[5.0]
     create_table :peliculas do |t|
       t.string :name
       t.integer :stars
-      t.string :main_actor
+      t.string :actor
       t.integer :year
-
+ 	  t.text :url
+      t.string :email_contact
+      t.boolean :oscar
+      t.string :category
+      t.string :director
+      t.string :country
+      t.integer :revenue_amount
+      t.string :genre
+      t.integer :precio
       t.timestamps
     end
   end
