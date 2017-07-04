a835ee4179ec6c7a7490101e067829743dcfac53
albertocuberes <albertocubkolau@gmail.com>
2017-07-04 17:52:11 +0200

    añadido fixed 13

--- db/migrate/20170704154616_add_ponercolumnas_to_peliculas.rb
+++ db/migrate/20170704154616_add_ponercolumnas_to_peliculas.rb
@@ -2,13 +2,13 @@ class AddPonercolumnasToPeliculas < ActiveRecord::Migration[5.0]
   def change
   #rename_column :peliculas, :main_actor, :actor
   add_column :peliculas, :email_contact, :string
-  add_column :peliculas, :url, :text
-  add_column :peliculas, :oscar, :boolean
-  add_column :peliculas, :category, :string
-  add_column :peliculas, :director, :string
-  add_column :peliculas, :country, :string
-  add_column :peliculas, :revenue_amount, :integer
-  add_column :peliculas, :genre, :string
-  add_column :peliculas, :precio, :integer  
+  # add_column :peliculas, :url, :text
+  # add_column :peliculas, :oscar, :boolean
+  # add_column :peliculas, :category, :string
+  # add_column :peliculas, :director, :string
+  # add_column :peliculas, :country, :string
+  # add_column :peliculas, :revenue_amount, :integer
+  # add_column :peliculas, :genre, :string
+  # add_column :peliculas, :precio, :integer  
   end
 end
