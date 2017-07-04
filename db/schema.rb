d55c035525bdf47f5f1703af20ef36767fbaf61c
albertocuberes <albertocubkolau@gmail.com>
2017-07-04 23:11:19 +0200

    add image_tag with link_to

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704154616) do
+ActiveRecord::Schema.define(version: 20170704203130) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
