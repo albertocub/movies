00bafa1d70de196152e7e7656f7b4db6ab64e6aa
albertocuberes <albertocubkolau@gmail.com>
2017-07-04 17:18:21 +0200

    añadido fixed 9

---
+++ db/migrate/20170704151723_add_columnas_to_peliculas.rb
@@ -0,0 +1,13 @@
+class AddColumnasToPeliculas < ActiveRecord::Migration[5.0]
+  def change
+  rename_column :main_actor, :actor
+  add_column :peliculas, :url, :text
+  add_column :peliculas, :oscar, :boolean
+  add_column :peliculas, :category, :string
+  add_column :peliculas, :director, :string
+  add_column :peliculas, :country, :string
+  add_column :peliculas, :revenue_amount, :integer
+  add_column :peliculas, :genre, :string
+  add_column :peliculas, :precio, :integer
+  end
+end
