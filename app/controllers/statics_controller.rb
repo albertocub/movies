4e5006dd691569a6dbf886b72a946a7271c1f8b4
albertocuberes <albertocubkolau@gmail.com>
2017-07-06 01:09:32 +0200

    views improves night

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -2,12 +2,15 @@ class StaticsController < ApplicationController
 	#skip_before_action :authenticate_user!
   def index
   	@peliculas = Pelicula.all
-  	@comentarios = Comentario.all 
+  	@comentarios = Comentario.all.order("created_at DESC").limit(6) 
   	if params[:search]
   	 @peliculas = Pelicula.search(params[:search]) 
    	elsif params[:search_year]
    	 @peliculas = Pelicula.search_year(params[:search_year]) 
-    elsif params[:search_genre]
+    end
+    if params[:search_genre] == "ALL"
+       @peliculas = Pelicula.all
+    elsif params[:search_genre] 
        @peliculas = Pelicula.search_genre(params[:search_genre]).order("created_at DESC")
     end	 
   end
